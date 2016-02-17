a4674388d39febce27bec87d48b7b3b2effef8a3
jonah sobol <jonah.sobol@gmail.com>
2016-02-17 10:09:28 -0800

    fix slack-caused issue

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-
 Video.delete_all
-​
+
 Video.create(
   embed_url:        "EPo5wWmKEaI",
   title:            "Give Me Everything",
@@ -16,7 +15,7 @@ Video.create(
   released_on:      Date.parse("6-5-2011"),
   tags:             "#dale"
 )
-​
+
 Video.create(
   embed_url:        "bTXJQ5ql5Fw",
   title:            "Time of Our Lives",
@@ -25,7 +24,7 @@ Video.create(
   released_on:      Date.parse("25-12-2011"),
   tags:             "#dale"
 )
-​
+
 Video.create(
   embed_url:        "SmM0653YvXU",
   title:            "Rain Over Me",
@@ -34,7 +33,7 @@ Video.create(
   released_on:      Date.parse("22-6-2011"),
   tags:             "#dale #marcanthony"
 )
-​
+
 Video.create(
   embed_url:        "hHUbLv4ThOo",
   title:            "Timber",
