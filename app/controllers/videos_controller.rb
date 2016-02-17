7d95088a13c163e85168747b5639a2e4c8279225
jonah sobol <jonah.sobol@gmail.com>
2016-02-17 15:51:08 -0800

    add ability to add

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,8 +1,22 @@
 class VideosController < ApplicationController
   def index
     @videos = Video.all
+  end
 
+  def new
+    @video = Video.new
+  end
 
+  def create
+    new_video = Video.new(self.video_params)
+    if new_video.save
+      redirect_to videos_path
+    else
+      render :new
+    end
+  end
 
+  def video_params
+    params.require(:video).permit(:title, :embed_url, :released_on)
   end
 end
