7b6672fbec11d3e479d911667ac802076d25c004
jonah sobol <jonah.sobol@gmail.com>
2016-02-17 21:31:03 -0800

    get more functionality out of Mr Worldwide

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,13 +3,17 @@ class VideosController < ApplicationController
     @videos = Video.all
   end
 
+  def show
+    @video = Video.find(params[:id])
+  end
+
   def new
     @video = Video.new
   end
 
   def create
-    new_video = Video.new(self.video_params)
-    if new_video.save
+    @video = Video.new(self.video_params)
+    if @video.save
       redirect_to videos_path
     else
       render :new
@@ -18,5 +22,24 @@ class VideosController < ApplicationController
 
   def video_params
     params.require(:video).permit(:title, :embed_url, :released_on)
+  end
+
+  def edit
+    @video = Video.find(params[:id])
+  end
+
+  def update
+    @video = Video.find(params[:id])
+    if @video.update_attributes(params.require(:video).permit(:title, :embed_url, :released_on))
+      redirect_to videos_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @video = Video.find(params[:id])
+    @video.destroy
+    redirect_to root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,12 @@ get "/videos/new" => "videos#new"
 
 post "/videos" => "videos#create"
 
+get "videos/:id/video" => "videos#edit", as: :edit_video
+
+patch "videos/:id" => "videos#update"
+
+delete "videos/:id" => "videos#destroy"
+
+get "videos/:id" => "videos#show", as: :video
+
 end
